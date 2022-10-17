c430358e40334f447af912d3973a98fee6910042
Marc-pro <iluobemarcel@gmail.com>
2022-10-17 02:43:37 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - print the string in the put function
  * this program prints "programming is like building a multilingualpuzzle
  * Return:0
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
