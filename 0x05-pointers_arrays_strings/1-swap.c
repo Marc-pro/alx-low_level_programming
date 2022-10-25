cf5a1f120444feea3f6d9ca4de8d34b15bb2c3d8
Marc-pro <iluobemarcel@gmail.com>
2022-10-24 22:09:37 -0700

    updates

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 /*
  *  * File: 1-swap.c
  *   * Auth: Brennan D Baraban
- *    */
+*/
 
 #include "main.h"
 
@@ -9,8 +9,8 @@
   *  * swap_int - Swaps the values of two integers.
   *   * @a: The first integer to be swapped.
   *    * @b: The second integer to be swapped.
-  *     */
- void swap_int(int *a, int *b)
+*/
+void swap_int(int *a, int *b)
 {
 		int tmp = *a;
 		*a = *b;
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,7 @@
  * *print_rev - Prints a string in reverse order
  * *@s: String to reverse
  * *Return: Nothing
- * */
+*/
 
 void print_rev(char *s)
 {
